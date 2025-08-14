04a7166039a6a046f39b00b757c0c8e4a8a52797
jeffdel2 <jeffdel2@gmail.com>
2025-08-13 20:41:29 -0400

    Updated to custom domain

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
   clientId: '0oap525ydtG2YwxWL1d7', // Replace with your Okta client ID
-  issuer: 'https://sherwindemo.oktapreview.com/oauth2/default', // Replace with your Okta domain
+  issuer: 'https://sherwindemo.twisec.com/oauth2/default', // Replace with your Okta domain
   redirectUri: window.location.origin + '/login/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true
